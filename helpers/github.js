aef60d4886a112c3579808843d13e09c85605538
Aschale <aschaled@gmail.com>
2021-03-31 15:52:55 -0500

    Completed getReposByUsername function

--- helpers/github.js
+++ helpers/github.js
@@ -1,19 +1,29 @@
 const axios = require('axios');
 const config = require('../config.js');
+const request = require('request');
 
-let getReposByUsername = (/* TODO */) => {
+let getReposByUsername = (username, callback) => {
+  let url = `https://api.github.com/users/${username}/repos`;
   // TODO - Use the axios module to request repos for a specific
   // user from the github API
 
-  // The options object has been provided to help you out, 
+  // The options object has been provided to help you out,
   // but you'll have to fill in the URL
   let options = {
-    url: 'FILL ME IN',
+    url: url,
     headers: {
       'User-Agent': 'request',
       'Authorization': `token ${config.TOKEN}`
     }
   };
+  // request(options, callback);
+  request.get(options, (err, res, body) => {
+    if (err) {
+      return console.log(err);
+    } else {
+      console.log(body);
+    };
+  });
 
 }
 
