cdce49b7c978d833b588819649980659934c706b
Aschale <aschaled@gmail.com>
2021-04-01 20:39:29 -0500

    Made repos name in the table linked to repo's page on GitHub

--- client/src/components/RepoListEntry.jsx
+++ client/src/components/RepoListEntry.jsx
@@ -4,7 +4,9 @@ import Search from './Search.jsx';
 
 const RepoListEntry = ({repo}) => (
   <li>
-   <span> {repo.id} </span><span> <a href={repo.html_url}>HTML Url</a></span><span> {repo.repo_name} </span><span> <a href={repo.git_url}>GIT Url</a></span><span> {repo.owner_name} </span><span> ({repo.updated_at} Updated Date) </span>
+   <span> {repo.id} </span>  <span>{repo.html_url}</span><span> <a href={repo.html_url}>{repo.repo_name}</a></span><span>{repo.git_url}</span><span> {repo.user_name} </span><span> ({repo.updated_at} Updated Date) </span>
   </li>
 )
 export default RepoListEntry;
+
+{/* <a href={repo.git_url}>GIT Url</a> */}
